95a12d17afd54487aa4106c8027630afd2287717
Felix Chandrakumar <i@fc.id.au>
2014-12-24 22:18:13 +1100

    Initial Commit

---
+++ CollapseCalcFrm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Collapse_Calculator
+{
+    partial class CollapseCalcFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CollapseCalcFrm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.neanPercent = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.statusLbl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BROWSE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Gray;
+            this.linkLabel1.Location = new System.Drawing.Point(274, 359);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(44, 13);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "y-str.org";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.Gray;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select File";
+            // 
+            // neanPercent
+            // 
+            this.neanPercent.Font = new System.Drawing.Font("OCR A Extended", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.neanPercent.ForeColor = System.Drawing.Color.Gray;
+            this.neanPercent.Location = new System.Drawing.Point(12, 42);
+            this.neanPercent.Name = "neanPercent";
+            this.neanPercent.Size = new System.Drawing.Size(306, 85);
+            this.neanPercent.TabIndex = 3;
+            this.neanPercent.Text = "0.00%";
+            this.neanPercent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("OCR A Extended", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(70, 127);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(188, 17);
+            this.result.TabIndex = 5;
+            this.result.Text = "pedigree collapsed";
+            // 
+            // statusLbl
+            // 
+            this.statusLbl.AutoSize = true;
+            this.statusLbl.Location = new System.Drawing.Point(13, 359);
+            this.statusLbl.Name = "statusLbl";
+            this.statusLbl.Size = new System.Drawing.Size(65, 13);
+            this.statusLbl.TabIndex = 11;
+            this.statusLbl.Text = "Select file ...";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Font = new System.Drawing.Font("OCR A Extended", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(16, 169);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(302, 170);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // CollapseCalcFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 385);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusLbl);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.neanPercent);
+            this.Controls.Add(this.result);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CollapseCalcFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pedigree Collapse Calculator";
+            this.Load += new System.EventHandler(this.CollapseCalcFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label neanPercent;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Label statusLbl;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ CollapseCalcFrm.cs
@@ -0,0 +1,237 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Collapse_Calculator
+{
+    public partial class CollapseCalcFrm : Form
+    {
+
+        long match_bp = 0;
+        string filename = null;
+        const long TOTAL_BP = 2881033286;
+        bool m_match_no_call = true;        
+        long m_BasePairs = 1000000;
+        long m_GapToBreak=100000;
+        long m_SNPs=150;
+        long m_AllowedErrors = 1;
+        Dictionary<int, string> message = new Dictionary<int, string>();
+        long m_match_error_count=0;
+        public CollapseCalcFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void CollapseCalcFrm_Load(object sender, EventArgs e)
+        {
+           m_AllowedErrors = (long)(m_SNPs * 4 / 100); //allow 4% errors
+
+           message.Add(0, "Your parents are not only siblings (incest) but their parents are also siblings (incest for multiple generations).");
+           message.Add(1, "Your parents are siblings (incest).");
+           message.Add(2, "Your parents are first cousins.");
+           message.Add(3, "Your parents are second cousins.");
+           message.Add(4, "Your parents are third cousins.");
+           message.Add(5, "Your parents are fourth cousins.");
+           message.Add(6, "Your parents are fifth cousins.");
+        }
+
+        public byte[] Zip(byte[] bytes)
+        {
+            using (var msi = new MemoryStream(bytes))
+            using (var mso = new MemoryStream())
+            {
+                using (var gs = new GZipStream(mso, CompressionMode.Compress))
+                {
+                    //msi.CopyTo(gs);
+                    CopyTo(msi, gs);
+                }
+
+                return mso.ToArray();
+            }
+        }
+
+        public byte[] Unzip(byte[] bytes)
+        {
+            using (var msi = new MemoryStream(bytes))
+            using (var mso = new MemoryStream())
+            {
+                using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+                {
+                    //gs.CopyTo(mso);
+                    CopyTo(gs, mso);
+                }
+
+                return mso.ToArray();
+            }
+        }
+
+        public void CopyTo(Stream src, Stream dest)
+        {
+            byte[] bytes = new byte[4096];
+
+            int cnt;
+
+            while ((cnt = src.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                dest.Write(bytes, 0, cnt);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                filename = dialog.FileName;
+                match_bp = 0;
+                neanPercent.Text = "0.00%";
+                neanPercent.ForeColor = Color.Gray;
+                statusLbl.Text = "Calculating ...";
+                button1.Enabled = false;
+                backgroundWorker2.RunWorkerAsync();
+            }
+        }
+
+        private string getAutosomalText(string file)
+        {
+            string text = null;
+
+            if (file.EndsWith(".gz"))
+            {
+                StringReader reader = new StringReader(Encoding.UTF8.GetString(Unzip(File.ReadAllBytes(file))));
+                text = reader.ReadToEnd();
+                reader.Close();
+
+            }
+            else if (file.EndsWith(".zip"))
+            {
+                using (var fs = new MemoryStream(File.ReadAllBytes(file)))
+                using (var zf = new ZipFile(fs))
+                {
+                    var ze = zf[0];
+                    if (ze == null)
+                    {
+                        throw new ArgumentException("file not found in Zip");
+                    }
+                    using (var s = zf.GetInputStream(ze))
+                    {
+                        using (StreamReader sr = new StreamReader(s))
+                        {
+                            text = sr.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            else
+                text = File.ReadAllText(file);
+            return text;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://www.y-str.org/");
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            StringReader reader = new StringReader(getAutosomalText(filename));
+            string line = null;
+            string[] data = null;            
+            long segment_start = 0;
+            long segment_end = 0;
+            string chr=null;
+            string pchr=null;
+            int i_chr=-1;
+            int snp_count = 0;
+            m_match_error_count = 0;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.StartsWith("#") || line.StartsWith("RSID"))
+                    continue;
+                line = line.Replace("\"", "").Replace("\t", ",");
+                data = line.Split(new char[] { ',' });
+                if (data.Length == 5)
+                    data[3] = data[3] + data[4];
+                
+                    chr=data[1];
+
+                if (!int.TryParse(chr, out i_chr))
+                    continue;
+                if (i_chr > 22 || i_chr <= 0)
+                    continue;
+
+                if (!isMatch(data[3], snp_count) || chr != pchr || long.Parse(data[2]) - segment_end > m_GapToBreak)
+                {
+                    if (segment_end - segment_start >= m_BasePairs && snp_count > m_SNPs)
+                        match_bp = match_bp + (segment_end - segment_start);
+
+                    segment_start = long.Parse(data[2]);
+                    snp_count = 0;
+                    m_match_error_count = 0;
+                }
+                segment_end = long.Parse(data[2]);
+                pchr=chr;
+                snp_count++;
+            }
+            reader.Close();
+        }
+
+        private bool isMatch(string p,int snp_count)
+        {
+            if (p == ReverseString(p))
+                return true;
+            if (p.IndexOfAny(new char[] { '0', '?', '-' }) != -1 && m_match_no_call)
+                return true;
+            m_match_error_count++;
+            if(m_match_error_count<m_AllowedErrors*snp_count/m_SNPs)
+                return true;
+            return false;
+        }
+
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }        
+
+        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            double percent = match_bp * 100.0 / TOTAL_BP;// excluding X chromosome
+            neanPercent.Text = percent.ToString("#0.00") + "%";
+            neanPercent.ForeColor = Color.Black;
+            statusLbl.Text = "";
+            button1.Enabled = true;
+
+            if (percent >= 50)
+            {
+                MessageBox.Show(message[0], "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                double total = 50;                
+                for (int i = 0; i < 100; i++)
+                {
+                    if (total / Math.Pow(2, i) > percent && percent > total / Math.Pow(2, i + 1))
+                    {
+                        if (total / Math.Pow(2, i) - percent > percent - total / Math.Pow(2, i + 1))
+                            MessageBox.Show(message[i + 1], "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        else
+                            MessageBox.Show(message[i], "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        break;
+                    }
+                }
+            }
+        }   
+    }
+}
